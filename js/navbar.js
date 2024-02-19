e9e222df13a6da80dffa9208f04250b20c1ef93b
Raezard77 <zartisian77@gmail.com>
2024-02-19 18:49:17 +0200

    Navbar #2

--- js/navbar.js
+++ js/navbar.js
@@ -1,18 +1,23 @@
 (function ($) {
     $(function () {
         $("nav ul li > a:not(:only-child)").click(function (e) {
-            $(this).siblings(".nav-dropdown").slideToggle();
-            $(".nav-dropdown").not($(this).siblings()).hide();
-            e.stopPropagation();
-        });
+            $(this).siblings(".nav-dropdown").slideToggle()
+            $(".nav-dropdown").not($(this).siblings()).hide()
+            e.stopPropagation()
+        })
         $("html").click(function () {
-            $(".nav-dropdown").hide();
-        });
+            $(".nav-dropdown").hide()
+        })
         $("#burger-menu").click(function () {
-            $("nav ul").slideToggle();
-        });
+            $("nav ul").slideToggle()
+        })
         $("#burger-menu").on("click", function () {
-            this.classList.toggle("active");
-        });
+            this.classList.toggle("active")
+        })
+        $(document).scroll(function () {
+            const $nav = $("header nav")
+            const $header = $("header")
+            $nav.toggleClass("scrolled", $(this).scrollTop() > $header.height() - ($header.height() / 10))
+        })
     });
-})(jQuery);
+})(jQuery)
